9ba430c7a00a12d1aec3ce66c96879233b9bc315
丁云鹏 <2798732983@qq.com>
2023-02-20 13:16:50 +0800

    add hi api

--- src/main/java/com/example/demo61/controller/TestController.java
+++ src/main/java/com/example/demo61/controller/TestController.java
@@ -1,2 +1,19 @@
-package com.example.demo61.controller;public class TestController {
+package com.example.demo61.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("hi")
+    public ResponseEntity hi(@RequestParam String s) {
+        return ResponseEntity.ok("hello " + s);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity create(@RequestParam String name,@RequestParam Integer age) {
+        return ResponseEntity.ok("hello " + name + ", 年龄" + age);
+    }
 }
